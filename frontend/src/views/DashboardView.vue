dab35aceb8d2a445f82a3b5c76e9f335bc5892ce
spcfranz-rgb <spcfranz@gmail.com>
2026-06-28 05:54:39 -0400

    Create DashboardView.vue

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <TopNav />
+    
+    <div class="row mb-4">
+      <div class="col-lg-6 mb-4 mb-lg-0">
+        <DeviceTable title="Network Switches" type="switches" :devices="store.devices.switches" />
+      </div>
+      <div class="col-lg-6">
+        <DeviceTable title="Network Video Recorders" type="nvrs" :devices="store.devices.nvrs" />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <DeviceTable title="Cameras" type="cameras" :devices="store.devices.cameras" @preview="openPreview" />
+      </div>
+    </div>
+
+    <WebRtcPreviewModal v-if="previewCam" :camera="previewCam" @close="previewCam = null" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TopNav from '../components/layout/TopNav.vue'
+import DeviceTable from '../components/monitoring/DeviceTable.vue'
+import WebRtcPreviewModal from '../components/modals/WebRtcPreviewModal.vue'
+import { useSystemStore } from '../stores/systemStore'
+
+const store = useSystemStore()
+const previewCam = ref(null)
+
+const openPreview = (cam) => {
+  previewCam.value = cam
+}
+</script>

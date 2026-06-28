29af06398a9079adfd0addd3b7a36739ac96218a
spcfranz-rgb <spcfranz@gmail.com>
2026-06-28 13:12:09 -0400

    Create DiagnosticsTab.vue

---
+++ frontend/src/components/dashboard/DiagnosticsTab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row g-4">
+    <div class="col-lg-6">
+      <PingTraceTool />
+    </div>
+    <div class="col-lg-6">
+      <SpeedtestTool />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PingTraceTool from '../diagnostics/PingTraceTool.vue'
+import SpeedtestTool from '../diagnostics/SpeedtestTool.vue'
+</script>

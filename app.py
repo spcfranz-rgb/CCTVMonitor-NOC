8472d0bfa05834832b32060ba603af2730937e8a
spcfranz-rgb <spcfranz@gmail.com>
2026-07-02 13:21:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 # Unauthorized copying of this file, via any medium, is strictly prohibited.
 # Proprietary and confidential.
 
-# Monkey patching must occur immediately after env vars but before standard libs
 import struct
 import select
 import eventlet
